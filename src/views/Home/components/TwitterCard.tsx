415f798dde3f80d6dd2a35962b54ab5095e615a5
Muffinswap <79991815+Muffinswap@users.noreply.github.com>
2021-03-05 00:38:22 +0700

    Update TwitterCard.tsx

--- src/views/Home/components/TwitterCard.tsx
+++ src/views/Home/components/TwitterCard.tsx
@@ -35,7 +35,7 @@ const TwitterCard = () => {
         <Timeline
           dataSource={{
             sourceType: 'profile',
-            screenName: 'GooseFinance'
+            screenName: 'MuffinFinance'
           }}
           options={{
             height: '300',

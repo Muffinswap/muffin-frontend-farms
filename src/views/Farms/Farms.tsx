b30eba8d16dcd79e3b8639d508b952dd67cbc129
Muffinswap <79991815+Muffinswap@users.noreply.github.com>
2021-03-09 15:10:23 +0700

    Update Farms.tsx

--- src/views/Farms/Farms.tsx
+++ src/views/Farms/Farms.tsx
@@ -94,13 +94,13 @@ const Farms: React.FC<FarmsProps> = (farmsProps) => {
       <Heading as="h1" size="lg" color="primary" mb="50px" style={{ textAlign: 'center' }}>
         {
           tokenMode ?
-            TranslateString(10002, 'Stake tokens to earn MFX')
+            TranslateString(10002, 'Stake tokens to earn MUFF')
             :
-          TranslateString(320, 'Stake LP tokens to earn MFX')
+          TranslateString(320, 'Stake LP tokens to earn MUFF')
         }
       </Heading>
       <Heading as="h2" color="secondary" mb="50px" style={{ textAlign: 'center' }}>
-        {TranslateString(10000, 'Deposit Fee will be used to buyback MFX')}
+        {TranslateString(10000, 'Deposit Fee will be used to buyback MUFF')}
       </Heading>
       <FarmTabButtons stakedOnly={stakedOnly} setStakedOnly={setStakedOnly}/>
       <div>

c107e95d737e93a5d1c0671f6a28d44a482bca57
Muffinswap <79991815+Muffinswap@users.noreply.github.com>
2021-03-11 12:03:10 +0700

    Update contracts.ts

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,11 +1,11 @@
 export default {
   cake: {
     56: '0xebc46c9898f675eefef43ec410dd91edbf0006c1',
-    97: '0xb61db4938c598f8d800fa9ca72963d247ef23beb',
+    97: '',
   },
   masterChef: {
     56: '0x26dA348628eF73C200Be6b2Ae80793EB668ccBD5',
-    97: '0xC90E2A9426459d41b4d6916a9068f99C96a124D7',
+    97: '',
   },
   wbnb: {
     56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
@@ -21,10 +21,10 @@ export default {
   },
   mulltiCall: {
     56: '0x55EdBF8D62B9beeA8148294511A5c1Fe835B840c',
-    97: '0x67ADCB4dF3931b0C5Da724058ADC2174a9844412',
+    97: '',
   },
   busd: {
     56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-    97: '0x337610d27c682e347c9cd60bd4b3b107c9d34ddd',
+    97: '',
   },
 }
